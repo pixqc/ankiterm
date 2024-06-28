8ad3cbe9693fa6fad2afa03251c79bff05b090ee
pix <pixqc1159@gmail.com>
2024-06-28 09:08:14 +0000

    make allocator on main, pass to fns

--- src/main.zig
+++ src/main.zig
@@ -2,6 +2,7 @@ const std = @import("std");
 const print = std.debug.print;
 const expect = std.testing.expect;
 const json = std.json;
+const ArrayList = std.ArrayList;
 
 const SRSAlgo = enum {
     sm2,
@@ -31,7 +32,12 @@ const Review = struct {
     algo: SRSAlgo,
 };
 
-fn getDefaultDeck() ![]u8 {
+const Deck = union {
+    card: Card,
+    review: Review,
+};
+
+fn getDefaultDeck(allocator: std.mem.Allocator) ![]u8 {
     const cards = [_]Card{
         .{ .id = 1, .front = "2^8", .back = "256" },
         .{ .id = 2, .front = "what is string in zig", .back = "pointer to null-terminated u8 array" },
@@ -44,7 +50,7 @@ fn getDefaultDeck() ![]u8 {
     };
 
     // create a buffer to write to
-    var buf = std.ArrayList(u8).init(std.heap.page_allocator);
+    var buf = std.ArrayList(u8).init(allocator);
     defer buf.deinit();
     const writer = buf.writer();
     for (cards) |card| {
@@ -68,55 +74,29 @@ fn writeDeck(filename: []const u8, deck: []u8) !usize {
     var writer = buffered_writer.writer();
     const file_size = try writer.write(deck);
     try buffered_writer.flush();
-    print("Successfully wrote {d} bytes to {s}.\n", .{ file_size, filename });
     return file_size;
 }
 
-pub fn readDeck(filename: []const u8) ![]u8 {
+fn readDeck(allocator: std.mem.Allocator, filename: []const u8) ![]u8 {
     const cwd = std.fs.cwd();
     const file = try cwd.openFile(filename, .{});
     defer file.close();
-    const allocator = std.heap.page_allocator;
     return try file.readToEndAlloc(allocator, std.math.maxInt(usize));
 }
-
-// pub fn review_deck(filename: []const u8) !void {
-//     // const cwd = std.fs.cwd();
-//     // const file = try cwd.openFile(filename, .{});
-//     // defer file.close();
-//     //
-//     // var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
-//     // defer arena.deinit();
-//     // const allocator = arena.allocator();
-//     // const file_contents = try file.readToEndAlloc(allocator, std.math.maxInt(usize));
-//     // defer allocator.free(file_contents);
-//     //
-//     // var lines = std.mem.split(u8, file_contents, "\n");
-//     var lines = readDeck(filename);
-//     while (lines.next()) |line| {
-//         if (line.len == 0) continue; // Skip empty lines
-//
-//         print("line: {s}\n", .{line});
-//         // try std.json.parseFromSlice(Card, allocator, line, .{}) catch |err| {
-//         //     try std.json.parseFromSlice(Review, allocator, line, .{}) catch |err2| {
-//         //         print("Error: {s}\n", .{err2});
-//         //         return err2;
-//         //     };
-//         //     return err;
-//         // };
-//     }
-// }
 
 pub fn main() !void {
 
     // SECTION: playground, for testing small code snippets ===================
 
-    if (true) {
-        const deck = try getDefaultDeck();
-        _ = try writeDeck("test.ndjson", deck);
-        const deck2 = try readDeck("test.ndjson");
-        print("{s}\n", .{deck});
-        print("{s}\n", .{deck2});
+    if (false) {
+        // const deck = try getDefaultDeck();
+        // _ = try writeDeck("test.ndjson", deck);
+        // const deck2 = try readDeck("test.ndjson");
+        // print("{s}\n", .{deck});
+        // print("{s}\n", .{deck2});
+        //
+        print("{s}\n", .{"hi playground"});
+        return;
     }
 
     // SECTION: main ==========================================================
@@ -140,6 +120,10 @@ pub fn main() !void {
         return;
     }
 
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+    const allocator = arena.allocator();
+
     const arg: []const u8 = std.mem.span(std.os.argv[1]);
     if (std.mem.eql(u8, arg, "init")) {
         if (std.os.argv.len < 3) {
@@ -147,36 +131,28 @@ pub fn main() !void {
             try stdout.print("Usage: {s} init <filename>\n", .{std.os.argv[0]});
             return;
         }
+
         const filename = std.mem.span(std.os.argv[2]);
-        const deck = try getDefaultDeck();
-        _ = try writeDeck(filename, deck);
+        const file = std.fs.cwd().openFile(filename, .{}) catch |err| switch (err) {
+            // only write if file doens't exist
+            error.FileNotFound => {
+                const deck = try getDefaultDeck(allocator);
+                const bytesWritten = try writeDeck(filename, deck);
+                try stdout.print("Successfully wrote {d} bytes to {s}.\n", .{ bytesWritten, filename });
+                return;
+            },
+            else => |e| return e,
+        };
+        defer file.close();
+        try stdout.print("Error: File '{s}' already exists. Choose a different filename or delete the existing file.\n", .{filename});
     } else if (std.mem.eql(u8, arg, "review")) {
         if (std.os.argv.len < 3) {
             try stdout.print("Error: 'review' command requires a filename.\n", .{});
             try stdout.print("Usage: {s} review <filename>\n", .{std.os.argv[0]});
             return;
         }
-        // const filename = std.mem.span(std.os.argv[2]);
-        // try review_deck(filename);
-    }
-    // else if (std.mem.eql(u8, arg, "stats")) {
-    //     if (std.os.argv.len < 3) {
-    //         try stdout.print("Error: 'stats' command requires a filename.\n", .{});
-    //         try stdout.print("Usage: {s} stats <filename>\n", .{std.os.argv[0]});
-    //         return;
-    //     }
-    //     const filename = std.mem.span(std.os.argv[2]);
-    //     try init(stdout);
-    // } else if (std.mem.eql(u8, arg, "tidy")) {
-    //     if (std.os.argv.len < 3) {
-    //         try stdout.print("Error: 'tidy' command requires a filename.\n", .{});
-    //         try stdout.print("Usage: {s} tidy <filename>\n", .{std.os.argv[0]});
-    //         return;
-    //     }
-    //     const filename = std.mem.span(std.os.argv[2]);
-    //     try init(stdout);
-    // }
-    else if (std.mem.eql(u8, arg, "--version")) {
+        try stdout.print("TODO: impl this", .{});
+    } else if (std.mem.eql(u8, arg, "--version")) {
         try stdout.print("0.0.0\n", .{});
     } else if (std.mem.eql(u8, arg, "--help")) {
         try stdout.print(help, .{std.os.argv[0]});
@@ -189,12 +165,16 @@ pub fn main() !void {
 // use `zig test src/main.zig` to run small code in isolation
 
 test "inits properly" {
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+    const allocator = arena.allocator();
+
     const filename = "test.ndjson";
-    const deck = try getDefaultDeck();
+    const deck = try getDefaultDeck(allocator);
     _ = try writeDeck(filename, deck);
 
     // read
-    const file_content = try readDeck(filename);
+    const file_content = try readDeck(allocator, filename);
     try expect(std.mem.eql(u8, file_content, deck));
 
     // cleanup
